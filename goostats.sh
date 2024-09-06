0747105b50bf0f39e37ba05612678e37d12b8ea7
Antonio Yu <antwyu@gmail.com>
2024-09-06 11:50:42 +0800

    Add welcome message and collect the filename

--- goostats.sh
+++ goostats.sh
@@ -1,3 +1,7 @@
 # Load a given file
 # Compute the min/max/range of values in a file
+fname=$1
+echo "working with file:"
+echo "Welcome to Nell's stats script"
 
+# Compute the min/max/range of values in a file.
