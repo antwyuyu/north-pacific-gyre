d5c8eb359c73290f67bef0b8aa21e98b692c7f38
antwyuyu <antwyu@gmail.com>
2024-09-06 15:34:22 +0800

    Calculate the max value

--- goostats.sh
+++ goostats.sh
@@ -5,4 +5,4 @@ echo "working with file: ${fname}"
 echo "Welcome to Nell's stats script"
 
 # Compute the min/max/range of values in a file.
-an ill-considered changes
+max=$( cat ${fname} | sort | tail -1 )
